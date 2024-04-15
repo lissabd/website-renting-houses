b699c474efcc2a4215c51a13f45566e4e9750fc4
Abdeeva Alisa <aabdeevaa@mail.ru>
2024-04-15 10:49:26 +0300

    create files for MainBanner

---
+++ src/components/MainBanner/MainBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./MainBanner.scss";
+
+
+const MainBanner = () => {
+  return (
+    <>
+       
+    
+    </>
+  );
+}
+
+export default MainBanner;
